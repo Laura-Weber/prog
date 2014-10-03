199f8814dba27478c41fed8cd5f23ae9f7a74417
Laura-Weber <Laura-Weber@users.noreply.github.com>
2014-10-03 14:24:08 +0200

    Update list_piece.h

--- list_piece.h
+++ list_piece.h
@@ -8,5 +8,17 @@ typedef struct piece_ptr{
   struct piece_ptr * next;
 } *list_piece;
 
+list_piece empty_list (void);
+list_piece add_piece(list_piece L, list_square S);
+int nb_piece(list_piece P);
+void up_piece(list_piece P);
+void down_piece(list_piece P);
+void up_piece(list_piece P);
+void right_piece(list_piece P);
+void left_piece(list_piece P);
+void creat_list_piece(list_piece P);
+list_piece rotate_r(list_piece P);
+list_piece rotate_l(list_piece P);
+    	
 #endif /* LIST_PIECE_H */
  
