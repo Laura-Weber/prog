3d9e8eb32fb3f98310d32d77e6145772fb36dd10
Laura-Weber <Laura-Weber@users.noreply.github.com>
2014-10-03 15:38:38 +0200

    Update list_piece.h
    plus lisible

--- list_piece.h
+++ list_piece.h
@@ -1,8 +1,21 @@
 #ifndef LIST_PIECE_H
 #define LIST_PIECE_H
 
+/******************************************************************/
+/* list_piece.c                                                   */
+/* Léa SCHMITT, Laura Weber                                       */
+/******************************************************************/
+
+/**************************************/
+/* Useful library inclusion           */
+/**************************************/
+
 #include "defs.h"
 #include "square.h"
+
+/*******************/
+/*    Structure    */
+/*******************/
 
 typedef struct piece_ptr{
   list_square square;
@@ -10,17 +23,65 @@ typedef struct piece_ptr{
   struct piece_ptr * next;
 } *list_piece;
 
+/*******************************************************************/
+/*             FUNCTION DEFINITION                                 */
+/*******************************************************************/
+
 list_piece empty_list (void);
-list_piece add_piece(list_piece L, list_square S);
-int nb_piece(list_piece P);
+
+/////////////////////
+//   add fuction   //
+/////////////////////
+
+void add_piece_before(list_piece L, list_square S);
+void add_piece_after(list_piece L, list_square S);
+void add_head_piece(list_piece racine, square S);
+void add_end_piece(list_piece racine, square S);
+
+/////////////////////////
+//     constructors    //
+/////////////////////////
+
+list_piece creat_list_piece();  
+
+/////////////////////////////
+//     Supp function       //
+/////////////////////////////
+
+void supp_piece(list_piece P);
+void supp_head_piece (list_piece racine);
+void supp_end_piece (list_piece racine);
+
+////////////////////////////
+//    access functions    //
+////////////////////////////
+
+list_piece head_piece(list_piece racine);
+list_piece end_piece(list_piece racine);
+
+////////////////////////////
+//      free function     //
+////////////////////////////
+
+void free_list_piece(list_piece P);
+
+//////////////////////////
+//    move function     //
+//////////////////////////
+
 void up_piece(list_piece P);
 void down_piece(list_piece P);
 void up_piece(list_piece P);
 void right_piece(list_piece P);
 void left_piece(list_piece P);
-void creat_list_piece(list_piece P);
 list_piece rotate_r(list_piece P);
 list_piece rotate_l(list_piece P);
-    	
+
+//////////////////////////
+//    other function    //
+//////////////////////////
+
+int nb_piece(list_piece P);
+
 #endif /* LIST_PIECE_H */
  
