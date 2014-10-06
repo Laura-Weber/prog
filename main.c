ba30df7c024643439423d38cb3631fcd8cbec265
Lea-Schmitt <lea_schmitt@ymail.com>
2014-10-06 09:36:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     SDL_WM_SetCaption("pentomino", NULL);
     
-    P = creat_list_piece();
+    //P = creat_list_piece();
     afficher_list(P);
     
     while (continuer)
