180263d5c5a138de96785b270dec65f421c8c2a9
Lea-Schmitt <lea_schmitt@ymail.com>
2014-11-14 15:34:54 +0100

    Create defs.h

---
+++ defs.h
@@ -0,0 +1,20 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+
+#include <SDL/SDL.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "list_square.h"
+#include "list_piece.h"
+#include "level.h"
+
+#define X_BOARD 200
+#define Y_BOARD 200
+#define BLOC_SIZE 32
+#define SCREEN_WIDTH  900
+#define SCREEN_HEIGHT 600
+
+#endif // DEFS_H
